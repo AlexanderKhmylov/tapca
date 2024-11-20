2ec84c5288a203b95d7ac09c06779ac4263c4f6c
MaksimKhmylov <maksimhmylov97@gmail.com>
2024-11-20 21:49:30 +0300

    tags done and cards model modified

--- tapcaproject/cards/admin.py
+++ tapcaproject/cards/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Card
+from .models import Card, Tag
 
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    list_display = ('word', 'transcription', 'translation')
-    list_editable = ('transcription', 'translation')
+    list_display = ('word', 'transcription', 'part_of_speech')
+    list_editable = ('transcription', 'part_of_speech')
     search_fields = ('word', 'translation')
+
+
+admin.site.register(Tag)
--- tapcaproject/cards/models.py
+++ tapcaproject/cards/models.py
@@ -1,11 +1,49 @@
 from django.db import models
 
+
+class Tag(models.Model):
+    COLOR_CHOICES = [
+        ('GREEN', 'Зеленый'),
+        ('RED', 'Красный'),
+        ('ORANGE', 'Оранжевый'),
+        ('BLUE', 'Синий'),
+        ('GRAY', 'Серый'),
+    ]
+    name = models.CharField(max_length=128, unique=True, verbose_name='Тег')
+    color = models.CharField(max_length=16, choices=COLOR_CHOICES, verbose_name='Цвет')
+
+    def __str__(self):
+        return f'{self.name} - {self.color}'
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'тег'
+        verbose_name_plural = 'Теги'
+
+
+
+
 class Card(models.Model):
+
+    PART_OF_SPEECH_CHOICES = [
+        ('verb', 'глагол'),
+        ('interjection', 'междометие'),
+        ('pronoun', 'местоимение'),
+        ('adverb', 'наречие'),
+        ('preposition', 'предлог'),
+        ('adjective', 'прилагательное'),
+        ('conjunction', 'союз'),
+        ('noun', 'существительное'),
+    ]
+
     word = models.CharField(
         max_length=128,
         unique=True,
         verbose_name='Слово'
     )
+    part_of_speech = models.CharField(
+        max_length=32, choices=PART_OF_SPEECH_CHOICES, verbose_name='Часть речи')
+    tag = models.ManyToManyField(Tag, verbose_name='Тег')
     transcription = models.CharField(
         max_length=128,
         verbose_name='Транскрипция',
@@ -15,9 +53,11 @@ class Card(models.Model):
     translation = models.TextField(verbose_name='Перевод')
 
     def __str__(self):
-        return self.word
+        return f'{self.word} - {self.part_of_speech}'
 
     class Meta:
         ordering = ('word',)
         verbose_name = 'карточка'
         verbose_name_plural = 'Карточки'
+
+
