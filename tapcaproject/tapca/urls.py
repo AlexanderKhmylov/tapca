f207d8c6e605d401e58a9782b0ed315cbc18d47d
MaksimKhmylov <maksimhmylov97@gmail.com>
2024-11-24 10:40:32 +0300

    text update

--- tapcaproject/tapca/urls.py
+++ tapcaproject/tapca/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path('', include('core.urls', namespace='core')),
-    path('cards/', include('cards.urls', namespace='cards')),
+    path('core/', include('core.urls', namespace='core')),
+    path('', include('cards.urls', namespace='cards')),
     path('admin/', admin.site.urls),
 ]
 
