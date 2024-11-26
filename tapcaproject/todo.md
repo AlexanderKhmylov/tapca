71bc146f918f2288f36d37c94d4da029a4b4d306
MaksimKhmylov <maksimhmylov97@gmail.com>
2024-11-26 21:15:11 +0300

    small changes

--- tapcaproject/todo.md
+++ tapcaproject/todo.md
@@ -1,4 +1,7 @@
 # TODO
 1. Add detail card view with slug
 2. Add form to add cards
-3. Upload to production
+3. Add table with all words and filters
+4. .env file
+5. Upload to production
+
--- tapcaproject/users/service.py
+++ tapcaproject/users/service.py
@@ -1,7 +1,6 @@
 import pyotp
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from django.conf import settings
 
 from .models import OTP
 from .config import OTP_DIGITS, OTP_VALID_INTERVAL_SEC
